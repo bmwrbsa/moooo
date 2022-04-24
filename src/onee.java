f9ff5eee46f2fab4234e542b4aabfebeb9532aed
Eliyahu Toronto <eliyahu1@edu.aac.ac.il>
2022-04-24 16:08:34 +0300

    first

--- src/onee.java
+++ src/onee.java
@@ -2,21 +2,10 @@ import javax.swing.*;
 
 public class onee extends JFrame {
     public static void main(String[] args) {
-        int a = 5;
-        int b =10;
-        System.out.println(a+b);
-        System.out.println("היי משה מה קורה למדתי איך להשתמש בגיט ביחד עם צווות אני ישב יללמד אותך ");
+        
 
     }
     onee one = new onee();
-
-
-     
-   
-      
-  
-
-
     public onee(){
      
 
