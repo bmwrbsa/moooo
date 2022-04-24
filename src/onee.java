00c0e3a6126a4eed7e3e9e74bb03787a68672332
eliyahu058327 <a3938081@gmail.com>
2022-04-24 14:44:56 +0300

    first

--- src/onee.java
+++ src/onee.java
@@ -1,17 +1,26 @@
+import javax.swing.*;
+
 public class onee extends JFrame {
     public static void main(String[] args) {
+        int a = 5;
+        int b =10;
+        System.out.println(a+b);
+    }
     onee one = new onee();
+
+
      
    
       
   
 
 
-    public onee() {
+    public onee(){
      
 
         // This creates a new window
         this.setSize(500, 500);
+
         this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         this.setLocationRelativeTo(null);
         this.setResizable(false);
